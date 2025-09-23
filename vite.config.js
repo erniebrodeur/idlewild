85f689b523b6387fcd706bbf0769b68edd35a997
Ernie Brodeur <ebrodeur@ujami.net>
2025-09-22 21:13:36 -0700

    fixed it, maybe

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === 'github-pages' ? '/idlewild/' : '/',
+  base: '/idlewild/',
   build: {
     outDir: 'dist'
   }
-}))
+})
